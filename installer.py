d64b12a20c7476081bbcf06f0b040bf5307d4481
Mr-Electeron <83303751+Mr-Electeron@users.noreply.github.com>
2021-05-02 17:48:02 +0430

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,7 @@
+import os
+os.system("python -m pip install pyfiglet")
+os.system("python -m pip install requests")
+os.system("python -m pip install pyfiglet")
+os.system("python -m pip install colorama")
+os.system("python -m pip install time")
+print("Installed Scuess!")
